a3fe39572cba9ff79ec54274bf85f53f26a5f2ec
Nikita Suslov <boyofanguish@yandex.ru>
2021-05-15 22:58:47 +0300

    auth

---
+++ htdocs/check.php
@@ -0,0 +1,28 @@
+<?
+// Скрипт проверки
+
+// Соединямся с БД
+require_once("mysql.php");
+
+if (isset($_COOKIE['id']) and isset($_COOKIE['hash']))
+{
+    $query = mysqli_query($link, "SELECT *,INET_NTOA(user_ip) AS user_ip FROM users WHERE id = '".intval($_COOKIE['id'])."' LIMIT 1");
+    $userdata = mysqli_fetch_assoc($query);
+
+    if(($userdata['user_hash'] !== $_COOKIE['hash']) or ($userdata['id'] !== $_COOKIE['id'])
+ or (($userdata['user_ip'] !== $_SERVER['REMOTE_ADDR'])  and ($userdata['user_ip'] !== "0")))
+    {
+        setcookie("id", "", time() - 3600*24*30*12, "/");
+        setcookie("hash", "", time() - 3600*24*30*12, "/", null, null, true); // httponly !!!
+        print "Хм, что-то не получилось";
+    }
+    else
+    {
+        print "Привет, ".$userdata['username'].". Всё работает!";
+    }
+}
+else
+{
+    print "Включите куки";
+}
+?>
--- htdocs/index.php
+++ htdocs/index.php
@@ -2,6 +2,7 @@
 	require_once("mysql.php");
 ?>
 
+
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
@@ -25,12 +26,14 @@
 			</div>
 			<div class="auth-section">
 				<span>Логин</span>
-				<input type="text" class="login-area" id="usernameInput" name="usernameInput1">
+				<input type="text" class="login-area" id="usernameInput" name="login">
 				<span>Пароль</span>
-				<input type="text" class="pass" id="passwordInput" name="passwordInput1">
+				<input type="text" class="pass" id="passwordInput" name="password">
+				<span>Не привязывать сессию к IP (не безопасно)</span>
+				<input type="checkbox" name="not_attach_ip">
 			</div>
 			<div class="enter-section">
-				<input type="submit" value="Войти" class="input-button" name="submitlogin">
+				<input type="submit" value="Войти" class="input-button" name="submit">
 			</div>
 			<div class="reg-section">
 				<a href="registration.php" class="reg-link">Регистрация</a>
@@ -40,28 +43,51 @@
 </body>
 </html>
 
-<?php 
+<?php
+	// Страница авторизации
 
-	$query = "SELECT * FROM auth_users_list";
-	$result = mysql_query($query) or die (mysql_error()); 
-	$row = mysql_fetch_row($result); 
-
-
-	#if(isset($_POST['usernameInput']) && isset($_POST['passwordInput'])){
-	if(isset($_POST['submitlogin']) && isset($_POST['usernameInput1']) && isset($_POST['passwordInput1'])){
-	    $username = $_POST['usernameInput'];    
-	    $password = $_POST['passwordInput'];
+	// Функция для генерации случайной строки
+	function generateCode($length=6) {
+	    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHI JKLMNOPRQSTUVWXYZ0123456789";
+	    $code = "";
+	    $clen = strlen($chars) - 1;
+	    while (strlen($code) < $length) {
+	            $code .= $chars[mt_rand(0,$clen)];
+	    }
+	    return $code;
 	}
-	else{
-	   echo("Please check username and password");
-	}
+	if(isset($_POST['submit']))
+	{
+	    // Вытаскиваем из БД запись, у которой логин равняеться введенному
+	    $query = mysqli_query($link,"SELECT id, passw FROM users WHERE username='".mysqli_real_escape_string($link,$_POST['login'])."' LIMIT 1");
+	    $data = mysqli_fetch_assoc($query);
 
-	if($username == $row ['username'] && $password == $row ['password']){
-	    
-	    echo("DONE!!!");
-	} 
-	else{
-	    
-	    echo("Username or password is wrong.");
-	} 
+	    // Сравниваем пароли
+	    if($data['passw'] === $_POST['password'])
+	    {
+	        // Генерируем случайное число и шифруем его
+	        $hash = md5(generateCode(10));
+
+	        if(!empty($_POST['not_attach_ip']))
+	        {
+	            // Если пользователя выбрал привязку к IP
+	            // Переводим IP в строку
+	            $insip = ", user_ip=INET_ATON('".$_SERVER['REMOTE_ADDR']."')";
+	        }
+
+	        // Записываем в БД новый хеш авторизации и IP
+	        mysqli_query($link, "UPDATE users SET user_hash='".$hash."' ".$insip." WHERE id='".$data['id']."'");
+
+	        // Ставим куки
+	        setcookie("id", $data['id'], time()+60*60*24*30, "/");
+	        setcookie("hash", $hash, time()+60*60*24*30, "/", null, null, true);
+
+	        // Переадресовываем браузер на страницу проверки нашего скрипта
+	        header("Location: check.php"); exit();
+	    }
+	    else
+	    {
+	        print ("Вы ввели неправильный логин/пароль");
+	    }
+	}
 ?>
