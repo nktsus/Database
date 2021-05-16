38809935e0f3bdfc60da7987c7229798c27cb242
Михаил Куликов <kulikov@MacBook-Air-Mihail.local>
2021-05-16 20:30:17 +0300

    work on profile_styles

--- htdocs/profile.php
+++ htdocs/profile.php
@@ -1,18 +1,11 @@
 <?php
 require_once("mysql.php");
 require_once("check1.php");
-print "Привет, ".$userdata['username']."";
-echo "<br>";
-print "Персональная информация:";
-echo "<br>";
-print "Имя: ".$userdata['firstname']."";
-echo "<br>";
-print "Фамилия: ".$userdata['lastname']."";
-echo "<br>";
-print "Email: ".$userdata['email']."";
-echo "<br>";
-print "Дата регистрации: ".$userdata['reg_date']."";
-echo "<br>";
+$html_login = "".$userdata['username']."";
+$html_name = "".$userdata['firstname']."";
+$html_lastname = "".$userdata['lastname']."";
+$html_email = "".$userdata['email']."";
+$html_reg_date = "".$userdata['reg_date']."";
 
 ?>
 
@@ -20,12 +13,25 @@ echo "<br>";
 <html>
 <head>
 	<title>Profile | SberMed</title>
+	<link rel="stylesheet" href="media/css/profile_styles.css">
 </head>
-<body>
-    <form method="POST" class="form-class">
-        <div class="title-container">
-            <a href="logout.php">Выйти из профиля</a>
-        </div>
-    </form>
+<body class="bg">
+	<div class="container">
+		<div class="person-logo">
+			<span class="hello">Привет, <?php echo $html_login;?></span>
+		</div>
+		<div class="info">
+			<span>Персональная информация</span>
+			<span>Имя: <?php echo $html_name;?></span>
+			<span>Фамилия:<?php echo $html_lastname;?></span>
+			<span>E-mail: <?php echo $html_email;?></span>
+			<span>Дата регистрации: <?php echo $html_reg_date;?></span>
+		</div>
+		<form method="POST" class="form-class">
+        	<div class="title-container">
+            	<a href="logout.php">Выйти из профиля</a>
+        	</div>
+    	</form>
+	</div>
 </body>
 </html>
