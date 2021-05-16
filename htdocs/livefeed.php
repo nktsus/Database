075f43fe84789c16cd3c3906d4dc8b4b98361316
Nikita Suslov <boyofanguish@yandex.ru>
2021-05-16 15:25:26 +0300

    livefeedposting

--- htdocs/livefeed.php
+++ htdocs/livefeed.php
@@ -2,9 +2,29 @@
 require_once("mysql.php");
 require_once("check1.php");
 print "Привет, ".$userdata['username']."";
+echo "<br>" ;
 if(isset($_POST['submit'])){
 	header("Location: createnews.php"); exit();
 }
+
+	//echo "<table border=1>";
+
+	$sql = mysqli_query($link, 'SELECT `n_title`,`n_text` FROM `news`');
+  	while ($result = mysqli_fetch_array($sql)) {
+  		echo "<br>" ;
+    	echo "{$result['n_title']}";
+    	echo "<br>" ;
+    	echo "{$result['n_text']}";
+    	echo "<br>";
+  	}
+
+	//while ($row=$result->fetch_assoc()){
+		//print"".$row['n_title']."";
+		//print"".$row['n_text']."";
+
+	//}	
+	
+	//echo "</table>";
 ?>
 
 <!DOCTYPE html>
